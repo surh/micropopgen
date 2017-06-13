0054923b32b109e73af3b25eb81c38dd732fcd26
surh <sur00mx@gmail.com>
2017-06-13 15:05:15 -0700

    Added util knit and stitch scripts from rhizogenomics repo.
    Added scripts to use knitr utilities. Also added script that reads
    SRA db database and gets a list of runs to download

---
+++ get_runs_hmp.r
@@ -0,0 +1,21 @@
+library(SRAdb)
+
+# Set up connection
+sqlfile <- "SRAmetadb.sqlite"
+dbcon <- dbConnect(SQLite(),sqlfile)
+
+# sraConvert(c('SRS049712','SRS049995'),'run',dbcon)
+
+# Read tables
+hmasm <- read.csv("~/micropopgen/data/HMP/HMASM.csv")
+hmasm.690 <- read.csv("~/micropopgen/data/HMP/HMASM-690.csv")
+hmiwgs <- read.csv("~/micropopgen/data/HMP/HMIWGS_healthy.csv")
+
+# Check consistency
+all(hmasm.690$SRS.ID %in% hmasm$SRS.ID)
+all(hmasm$SRS.ID %in% hmiwgs$SRS.ID)
+
+# Get runs
+res <- sraConvert(hmiwgs$SRS.ID, c("sra"),dbcon)
+
+write.table(res, "hmiwgs_run_table.txt", quote = FALSE, col.names = TRUE, row.names = FALSE,sep = "\t")
