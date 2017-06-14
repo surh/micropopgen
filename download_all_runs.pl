30ed944a69755a00145a35cbe6f6532fdb624d0d
surh <sur00mx@gmail.com>
2017-06-14 10:54:07 -0700

    Script creates submissions files. One per sample or 1260 in the case of the HMP WGS data.
    Now need to save list of submission files and send it to qsub.

--- download_all_runs.pl
+++ download_all_runs.pl
@@ -1,8 +1,19 @@
 #!/usr/bin/env perl
+# Copyright (C) 2017 Sur Herrera Paredes
+
+# Reads a table with sample and run columns, and dowloads all the run
+# fastq files from SRA.
+
+# Currently it keeps all temporary files
+
+# It only uses qsub for submitting to cluster
+
+# Creates one submission per sample
 
 use strict;
 use warnings;
 use Getopt::Long;
+use File::Temp qw/ tempfile tempdir/;
 
 my $infile = '';
 my $outdir = '';
@@ -10,13 +21,17 @@ my $method = 'qsub';
 my $run_col = 5;
 my $sample_col = 1;
 my $skip = 1;
+my $split_by = 'sample';
+my $logdir = 'logs/';
 
 my $opts = GetOptions('infile|i=s' => \$infile,
 			'outdir|o=s' => \$outdir);
 
 $sample_col--;
 $run_col--;
-$runs_ref = process_run_list($infile,$sample_col,$run_col);
+$logdir =  "$outdir/$logdir/";
+my $runs_ref = process_run_list($infile,$sample_col,$run_col);
+create_qsub_submission($runs_ref,$outdir);
 
 ### Subroutines
 sub process_run_list{
@@ -45,8 +60,48 @@ sub process_run_list{
 
 
 sub create_qsub_submission{
-	my($runs_ref,$outdir) = @_A
+	my($runs_ref,$outdir) = @_;
+	
+	# Set working space
+	mkdir $outdir unless -d $outdir;
+
+	my $submissions_dir = tempdir( "submissions.XXXX", DIR => $outdir);
+	print "Saving submission files to $submissions_dir\n";
+
+	# Extract list of samples and pass it to function that creates single submission file
+	if ($split_by eq 'sample'){
+		my $sample;
+		for $sample (keys %$runs_ref){
+			my $run_list_ref = $runs_ref->{$sample};
+			my $sample_file = create_single_submission_file($sample,$run_list_ref,
+									$submissions_dir);
+		}
+	}else{
+		die "Only split by sample is allowed for now \n";
+	}
 }
 
+sub create_single_submission_file{
+	my ($name,$run_list_ref,$submissions_dir) = @_;
+
+	# Create new submission file
+	my $submission_file = "$submissions_dir/$name.submission.bash";
+	open(SUB,'>',$submission_file) or die "Can't create $submission_file ($!)";
+	print SUB "#!/bin/bash\n";
+	print SUB "#PBS -N download.$name\n";
+	print SUB "#PBS -d $outdir\n";
+	print SUB "#PBS -o $logdir/download.$name.log\n";
+	print SUB "#PBS -e $logdir/download.$name.err\n";
+	print SUB "#PBS -l mem=1000mb\n";
+	print SUB "module load sra-tools/2.8.0\n";
+
+	# Add lines for every run in sample
+	for (@$run_list_ref){
+		print SUB "fastq-dump $outdir $_ --gzip\n";
+	}
+	close SUB;
+
+	return($submission_file);
+}
 
 
