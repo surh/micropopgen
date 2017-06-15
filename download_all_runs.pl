daa6654874a38ec85c359b456243b37b38555cc7
surh <sur00mx@gmail.com>
2017-06-15 16:43:42 -0700

    Adding support for bash submission on head node

--- download_all_runs.pl
+++ download_all_runs.pl
@@ -14,6 +14,7 @@ use strict;
 use warnings;
 use Getopt::Long;
 use File::Temp qw/ tempfile tempdir/;
+use POSIX qw/ ceil floor/;
 
 my $infile = '';
 my $outdir = '';
@@ -23,6 +24,7 @@ my $sample_col = 1;
 my $skip = 1;
 my $split_by = 'sample';
 my $logdir = 'logs/';
+my $ngroups = 2;
 
 my $opts = GetOptions('infile|i=s' => \$infile,
 			'outdir|o=s' => \$outdir,
@@ -34,11 +36,13 @@ $sample_col--;
 $run_col--;
 $logdir =  "$outdir/$logdir/";
 my $runs_ref = process_run_list($infile,$sample_col,$run_col);
-my $submission_list_ref = create_submission_sets($runs_ref,$outdir,$split_by);
+my $submission_list_ref = create_submission_sets($runs_ref,$outdir,$split_by,$ngroups);
 if($method eq 'qsub'){
 	qsub_submissions($submission_list_ref,$logdir);
+}elsif($method eq 'bash'){
+	run_command($_) foreach @$submission_list_ref;
 }else{
-	die "Only method qsub is allowed for the momment.\n";
+	die "Method ($method) not recognized.\n";
 }
 
 ### Subroutines
@@ -68,7 +72,7 @@ sub process_run_list{
 
 
 sub create_submission_sets{
-	my($runs_ref,$outdir,$split_by) = @_;
+	my($runs_ref,$outdir,$split_by,$ngroups) = @_;
 	
 	# Set working space
 	mkdir $outdir unless -d $outdir;
@@ -79,6 +83,7 @@ sub create_submission_sets{
 	# Extract list of samples and pass it to function that creates single submission file
 	my @submission_list;
 	if ($split_by eq 'sample'){
+		print "== Entering split by sample\n";
 		my $sample;
 		for $sample (keys %$runs_ref){
 			my $run_list_ref = $runs_ref->{$sample};
@@ -87,8 +92,44 @@ sub create_submission_sets{
 									$logdir,$outdir);
 			push(@submission_list,$sample_file);
 		}
+	}elsif($split_by eq 'groups'){
+		#print "== Entering split by number of groups\n";
+		my @samples = keys %$runs_ref;
+		my $total_samples = scalar @samples;
+		my $samples_per_submission = ceil($total_samples / $ngroups);
+		my $sample;
+		my $group_list_ref = {};
+		my $i = 0;
+		my $group_i = 0;
+		for $sample (@samples){
+			#print "====Sample:$sample\n";
+			if (($i % $samples_per_submission) == 0){
+				$group_i++;	
+				$group_list_ref->{"group.$group_i"} = [@{$runs_ref->{$sample}}];
+				#print "$group_i\n";
+				my @temp = @{$group_list_ref->{"group.$group_i"}};
+				#print "group:" . "@temp" . "\n";
+				
+			}else{
+				push(@{$group_list_ref->{"group.$group_i"}},@{$runs_ref->{$sample}});	
+			}
+			#print "@{$runs_ref->{$sample}}\n";
+			#print "===$i\n";
+			$i++;
+		}
+			
+		my $group;
+		for $group (keys %$group_list_ref){
+			#my @temp = @{$group_list_ref->{$group}};
+			#print "$group " . scalar(@temp) . "\n";
+		    #print "$group " . "@temp" . "\n";
+		    my $sample_file = create_single_submission_file($group,$group_list_ref->{$group},
+									$submissions_dir,
+									$logdir,$outdir);
+			push(@submission_list,$sample_file);
+		}
 	}else{
-		die "Only split by sample is allowed for now \n";
+		die "Unrecognize split_by ($split_by) \n";
 	}
 
 	return(\@submission_list);
@@ -131,7 +172,7 @@ sub qsub_submissions{
 	mkdir $logdir unless -d $logdir;
 
 	my $file;
-	run_command("qsub $_") foreach @$submission_list_ref;
+	run_command("qsub $_") foreach @$submission_list_ref[0..4];
 
 	print "==========SUBMISSIONS DONE==========\n";
 }
