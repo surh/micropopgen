9118dfb1fb300274c8a50256146c66aa7814e280
surh <sur00mx@gmail.com>
2017-06-28 17:45:19 -0700

    loading function

--- generate_sample_fastq.pl
+++ generate_sample_fastq.pl
@@ -17,7 +17,7 @@ use File::Temp qw/ tempfile tempdir/;
 use File::Basename qw(dirname);
 use Cwd  qw(abs_path);
 use lib dirname(abs_path $0) . '/lib';
-use sra qw(process_run_list);
+use sra qw(process_run_list match_sra_files_in_dir);
 
 my $mapfile = '';
 my $indir = '';
