b2065a94ebd492106b8426c5a2170ad5a39366a3
surh <sur00mx@gmail.com>
2017-07-05 15:07:34 -0700

    midas2 on sample 2

--- run_midas2_sample2.sh
+++ run_midas2_sample2.sh
@@ -4,4 +4,4 @@
 #PBS -e logs/midas2_sample2.err
 #PBS -l mem=2000mb
 module load MIDAS
-run_midas.py species midas2/sample1 -1 /godot/hmp/WGS/temp/test/SRS103943 -2 SRS103943.denovo_duplicates_marked.trimmed.2.fastq
+run_midas.py species midas2/sample1 -1 SRS103943.denovo_duplicates_marked.trimmed.1.fastq -2 SRS103943.denovo_duplicates_marked.trimmed.2.fastq
