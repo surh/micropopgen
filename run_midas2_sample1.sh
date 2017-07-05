f0613e8611b9936bf2938014afdb5ab8f8952482
surh <sur00mx@gmail.com>
2017-07-05 15:25:40 -0700

    sample2 midas2

--- run_midas2_sample1.sh
+++ run_midas2_sample1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#PBS -N MIDAS2
+#PBS -N MIDAS2.2
 #PBS -o logs/midas2_sample1.log
 #PBS -e logs/midas2_sample1.err
 #PBS -l mem=2000mb
 module load MIDAS
-run_midas.py species midas2/sample1 -1 /godot/hmp/WGS/temp/test/SRS013216.tar.bz2
+run_midas.py species midas2/sample1 -1 /godot/hmp/WGS/temp/test/SRS013216/SRS013216.fastq
