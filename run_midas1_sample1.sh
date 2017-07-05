15c6a87e8de4039a430b78978c5af460f692ae4a
surh <sur00mx@gmail.com>
2017-07-05 14:45:54 -0700

    removing directory

--- run_midas1_sample1.sh
+++ run_midas1_sample1.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 #PBS -N MIDAS1
-#PBS -d ~/micropopgen/exp/2017/today4/
-#PBS -o ~/micropopgen/exp/2017/today4/logs/midas1_sample1.log
-#PBS -e logs/download.err
+#PBS -o logs/midas1_sample1.log
+#PBS -e logs/midas1_sample1.err
 #PBS -l mem=2000mb
 module load MIDAS
 run_midas.py species midas1/sample1 -1 /godot/hmp/WGS/temp/temp_fastq/SRS011061_R1.fastq.gz  -2 /godot/hmp/WGS/temp/temp_fastq/SRS011061_R2.fastq.gz
--- run_midas2_sample2.sh
+++ run_midas2_sample2.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 #PBS -N MIDAS2
-#PBS -d ~/micropopgen/exp/2017/today4/
-#PBS -o ~/micropopgen/exp/2017/today4/logs/midas2_sample2.log
-#PBS -e logs/download.err
+#PBS -o logs/midas2_sample2.log
+#PBS -e logs/midas2_sample2.err
 #PBS -l mem=2000mb
 module load MIDAS
 run_midas.py species midas2/sample1 -1 /godot/hmp/WGS/temp/test/SRS103943.tar.bz2
