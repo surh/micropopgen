3f1c23d805b6e0d223a967995f8dd4e35f05f3be
surh <sur00mx@gmail.com>
2017-07-05 13:55:37 -0700

    qsub scripts for running midas

---
+++ run_midas1_sample2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#PBS -N MIDAS1
+#PBS -d ~/micropopgen/experiments/2017/today4/
+#PBS -o ~/micropopgen/experiments/2017/today4/logs/midas1_sample2.log
+#PBS -e logs/download.err
+#PBS -l mem=2000mb
+module load MIDAS
+run_midas.py species midas1/sample2 -1 /godot/hmp/WGS/temp/temp_fastq/SRS011098_R1.fastq.gz  -2 /godot hmp/WGS/temp/temp_fastq/SRS011098_R2.fastq.gz
---
+++ run_midas2_sample2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#PBS -N MIDAS2
+#PBS -d ~/micropopgen/experiments/2017/today4/
+#PBS -o ~/micropopgen/experiments/2017/today4/logs/midas2_sample2.log
+#PBS -e logs/download.err
+#PBS -l mem=2000mb
+module load MIDAS
+run_midas.py species midas2/sample1 -1 /godot/hmp/WGS/temp/test/SRS103943.tar.bz2
