b941888d54b37f174e79e06713944a825ce203a4
surh <sur00mx@gmail.com>
2017-07-14 15:32:12 -0700

    parameters

--- hmp_metadata/download_runs.py
+++ hmp_metadata/download_runs.py
@@ -14,24 +14,32 @@
 
 # Adapted from download_all_runs.pl script
 
+
+# print(__name__)
 # Run if called as script
-if __name__ == '__main__':
+if __name__ == "__main__":
     import argparse
-    
-    # Set argument defaults
-    infile = ''
-    outdir = ''
-    method = 'qsub'
-    run_col = 5
-    sample_col = 1
-    #skip = 1
-    header = True
-    split_by = 'sample'
-    logdir = 'logs/'
-    ngroups = 2
-    
+        
     # Define arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument("--infile","-i", help = "Input tab delimited file that maps runs (SRR) and samples (SRS)",
+    required = parser.add_argument_group("Required arguments")
+    required.add_argument("--infile","-i", help = "Input tab delimited file that maps runs (SRR) and samples (SRS)",
                         type = str)
-    
+    parser.add_argument("--outdir","-o", help = "Output directory", type = str, default = "./", required = True)
+    parser.add_argument("--method","-m", help = "method to submit jobs to the server", type = str,
+                        default = 'qsub', choices = ['qsub','bash'])
+    parser.add_argument("--sample_col", help = "Column number where the sample ID (SRS) is stored", type = int,
+                        default = 5)
+    parser.add_argument("--header", help = "Flag indicating wether table has headers in the first row",
+                        action = "store_true")
+    parser.add_argument("--split_by", help = "Method to split runs for download", default = 'sample',
+                        choices = ['sample'])
+    parser.add_argument('--logdir','-l', help = "Directory where to store log of run", default = 'logs',
+                        type = str)
+    parser.add_argument('--ngroups', help = "Number of groups to divide the runs into. Equal to the number of jobs that will be submitted",
+                        default = 2, type = int)
+    args = parser.parse_args()
+
+
+
+
