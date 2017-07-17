a53aea7f0dab15155a8dcd94b59c2e7ddacfa941
surh <sur00mx@gmail.com>
2017-07-17 12:31:57 -0700

    adding HMAMS to metadata

--- hmp_metadata/2.merge_all_metadata.r
+++ hmp_metadata/2.merge_all_metadata.r
@@ -1,11 +1,11 @@
 # Load metadata from metagenome samples
-file <- "~/micropopgen/data/HMP/HMIWGS_healthy_run_metadata/all_runs.txt"
-all_runs <- read.table(file,header = TRUE,sep = "\t")
-head(all_runs)
+# file <- "~/micropopgen/data/HMP/HMIWGS_healthy_run_metadata/all_runs.txt"
+# all_runs <- read.table(file,header = TRUE,sep = "\t")
+# head(all_runs)
 
-file <- "~/micropopgen/data/HMP/HMIWGS_healthy.csv"
-hmiwgs_samples <- read.table(file,sep = ",", quote = '"', header = TRUE)
-head(hmiwgs_samples)
+# file <- "~/micropopgen/data/HMP/HMIWGS_healthy.csv"
+# hmiwgs_samples <- read.table(file,sep = ",", quote = '"', header = TRUE)
+# head(hmiwgs_samples)
 
 file <- "~/micropopgen/data/HMP/HMASM.csv"
 hmasm_samples <- read.table(file,sep = ",", quote = '"', header = TRUE)
@@ -15,9 +15,16 @@ file <- "~/micropopgen/data/HMP/HMASM-690.csv"
 hmasm_samples_690 <- read.table(file,header = TRUE,sep = ",",quote = '"')
 head(hmasm_samples_690)
 
-file <- "subject_metadata.txt"
-ssu <- read.table(file,header = TRUE,sep = "\t",quote = '')
-head(ssu)
+file <- "~/micropopgen/data/HMP/hmp_catalogue_wgs_all_metadata.csv"
+catalogue <- read.csv(file)
+head(catalogue)
+
+# file <- "subject_metadata.txt"
+# ssu <- read.table(file,header = TRUE,sep = "\t",quote = '')
+# head(ssu)
+
+# The first thing is to identify which samples are in the original publication,
+# and which passed QC
 
 # Compare body sites from hmasm sets
 sites1 <- as.character(hmasm_samples_690$Body.Site)
@@ -38,53 +45,17 @@ if(!all(sites1[ shared ] == sites2[ shared ])){
 
 hmasm_samples$HMASM.QC <- !is.na(sites1[ as.character(hmasm_samples$SRS.ID) ])
 table(hmasm_samples$HMASM.QC)
+head(hmasm_samples)
 #row.names(hmasm_samples) <- as.character(hmasm_samples$SRS.ID)
 
-# Compare all samples with hmasm samples
-sites1 <- as.character(hmasm_samples$Body.Site)
-names(sites1) <- as.character(hmasm_samples$SRS.ID)
-sites2 <- as.character(hmiwgs_samples$Body.Site)
-names(sites2) <- as.character(hmiwgs_samples$SRS.ID)
-
-head(sites1)
-head(sites2)
-
-shared <- intersect(names(sites1),names(sites2))
-
-if(!all(sites1[ shared ] == sites2[ shared ])){
-  stop("ERROR: Not all samples match on body site")
-}else{
-  cat("All samples match\n")
-}
-
-# Add publication and QC status
+# Add information about puiblication and QC into main table
+ftable(HMP.Isolation.Body.Site ~ HMP.Isolation.Body.Subsite, catalogue)
 row.names(hmasm_samples) <- as.character(hmasm_samples$SRS.ID)
-hmiwgs_samples$HMASM <- as.character(hmiwgs_samples$SRS.ID) %in% as.character(hmasm_samples$SRS.ID)
-hmiwgs_samples$HMASM.QC <- hmasm_samples[ as.character(hmiwgs_samples$SRS.ID), "HMASM.QC" ]
-hmiwgs_samples$Reads.File.Location <- NULL
-hmiwgs_samples$Reads.File.MD5 <- NULL
-hmiwgs_samples$Reads.File.Size <- NULL
-head(hmiwgs_samples)
+catalogue$HMASM <- as.character(catalogue$Sequence.Read.Archive.ID) %in% row.names(hmasm_samples)
+catalogue$HMASM.QC <- hmasm_samples[ as.character(catalogue$Sequence.Read.Archive.ID), "HMASM.QC" ]
+table(catalogue$HMASM, useNA = "always")
+table(catalogue$HMASM.QC, useNA = "always")
 
-# table(hmasm_samples[ as.character(hmiwgs_samples$SRS.ID), "HMASM.QC" ], useNA = "always")
-# hmasm_samples[ as.aracter(all_runs$secondary_sample_accession), "Body.Site"]
-
-# HERE ADD METADATA FROM 16S
-# head(ssu)
-# row.names(ssu) <- as.character(ssu$SRS.ID)
-# hmiwgs_samples$RSID <- ssu [as.character(hmiwgs_samples$SRS.ID), "RSID" ] 
-# table(hmiwgs_samples$RSID,useNA = "always")
-# 
-# hmiwgs_samples$Sex <- ssu [as.character(hmiwgs_samples$SRS.ID), "Sex" ] 
-# table(hmiwgs_samples$Sex,useNA = "always")
-# 
-# hmiwgs_samples$Body.Subsite <- ssu [as.character(hmiwgs_samples$SRS.ID), "Body.Subsite" ] 
-# table(hmiwgs_samples$Body.Subsite,useNA = "always")
-# 
-# hmiwgs_samples$Visit.No<- ssu [as.character(hmiwgs_samples$SRS.ID), "Visit.No" ] 
-# table(hmiwgs_samples$Visit.No,useNA = "always")
-# 
-# ftable(Body.Site ~ Body.Subsite, ssu)
 
 # Add body subsite
 hmiwgs_samples$Body.Subsite <- NA
