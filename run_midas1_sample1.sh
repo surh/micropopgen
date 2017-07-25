07454c8230651e5e2c58840c00e72b2a4dbdabc0
surh <sur00mx@gmail.com>
2017-07-24 17:58:33 -0700

    deleting old files

--- run_midas1_sample1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#PBS -N MIDAS1.1
-#PBS -d /home/sur/micropopgen/exp/2017/today4/
-#PBS -o logs/midas1_sample1.log
-#PBS -e logs/midas1_sample1.err
-#PBS -l mem=2000mb
-module load MIDAS
-pwd
-run_midas.py species midas1/sample1 -1 /godot/hmp/WGS/temp/temp_fastq/SRS011061_R1.fastq.gz  -2 /godot/hmp/WGS/temp/temp_fastq/SRS011061_R2.fastq.gz
--- run_midas2_sample2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#PBS -N MIDAS2.2
-#PBS -d /home/sur/micropopgen/exp/2017/today4/
-#PBS -o logs/midas2_sample2.log
-#PBS -e logs/midas2_sample2.err
-#PBS -l mem=5000mb
-module load MIDAS
-pwd
-run_midas.py species midas2/sample1 -1 /godot/hmp/WGS/temp/test/SRS103943/SRS103943.denovo_duplicates_marked.trimmed.1.fastq -2 /godot/hmp/WGS/temp/test/SRS103943/SRS103943.denovo_duplicates_marked.trimmed.2.fastq
