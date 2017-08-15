04457f24b4c6ba0b09eb55886d75129d85be77b4
surh <sur00mx@gmail.com>
2017-08-15 16:12:44 -0700

    CalledProcessError exception needs more parameters

--- hmp_metadata/errocatch.py
+++ hmp_metadata/errocatch.py
@@ -1,16 +1,20 @@
+import subprocess
+from subprocess import CalledProcessError
+
 def call_cmd(cmd):
     """Call aspera to download runs from SRA"""
     
     print("\n=============================================")
     command = cmd
     print(command)
+    FAILED = []
     try:
         check = run_command(command)
         if check.returncode != 0:
             print("Raising error")
-            raise CalledProcessError("Aspera download failed for run {}".format(cmd))
+            raise CalledProcessError(check.returncode,command,check.stdout,check.stderr)
     except CalledProcessError:
-        print("\tWARNING: Failed downloading run {}".format(run))
+        print("\tWARNING: Failed command {}".format(command))
         FAILED.append([1, 'dummy'])
     print("=============================================\n\n")
     return(FAILED)
@@ -18,13 +22,18 @@ def call_cmd(cmd):
 def run_command(command):
     status = 0;
     print("Executing:\n>{}".format(command))
-    status = subprocess.run(command, shell = True)
-    print("Status={}\n\n".format(status));
-
+    try:
+        status = subprocess.run(command, shell = True)
+        print("Status={}\n\n".format(status));
+    except:
+        print("\tWARNING")
+        
+    print("run_command done")
     return(status)
 
 if __name__ == "__main__":
     cmd = "ls -la"
+    cmd = 'a'
     fail = call_cmd(cmd)
     print(fail)
     
