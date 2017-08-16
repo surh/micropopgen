8ed7b54ccf731ef46cf5cdbd3e35d3931a614227
surh <sur00mx@gmail.com>
2017-08-16 14:26:22 -0700

    testing not hanging until done

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import fyrd
 
 command1 = "ls -la"
@@ -12,17 +13,19 @@ command2 = "sleep 10"
 
 
 
-commands = ['ls -la /', 'ls -la /home/', 'pwd', 'ls $SCRATHC', 'ls $PI_HOME']
+commands = ['ls -la /', 'ls -la /home/', 'pwd',
+            'ls $SCRATHC', 'ls $PI_HOME','sleep 30',
+            'sleep 30', 'sleep 30']
 out = []
 for cmd in commands:
     out.append(fyrd.submit(cmd, runpath = "run", outpath = "out",
                            clean_files = False, clean_outputs = False,
                            mem = '10MB', nodes = 1, time ='00:30:00',
-                           threads = 1, partition = None))
+                           threads = 1, partition = hbfraser))
     print(cmd) 
 
-for i in out:
-    print(i.get())
+# for i in out:
+#     print(i.get())
 
     
 
