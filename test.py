059d613ae043500d0b30a429d260760ac7c48ea5
surh <sur00mx@gmail.com>
2017-08-16 15:27:24 -0700

    testing max_jobs

--- test.py
+++ test.py
@@ -3,12 +3,15 @@ import fyrd
 
 commands = ['sleep 180','sleep 180','sleep 180',
             'sleep 180', 'sleep 180', 'sleep 180']
-out = []
+JOBS = []
 for cmd in commands:
-    out.append(fyrd.submit(cmd, runpath = "run", outpath = "out",
-                           clean_files = False, clean_outputs = False,
-                           mem = '10MB', nodes = 1, time ='00:30:00',
-                           cores = 1, partition = 'hbfraser',
-                           scriptpath = 'sub', outfile = 'test.log',
-                           errfile = 'test.err'))
-    print(cmd) 
+    JOBS.append(fyrd.Job(cmd, runpath = "run", outpath = "out",
+                         clean_files = False, clean_outputs = False,
+                         mem = '10MB', nodes = 1, time ='00:30:00',
+                         cores = 1, partition = 'hbfraser',
+                         scriptpath = 'sub', outfile = 'test.log',
+                         errfile = 'test.err', max_jobs = 2))
+    
+    JOBS[-1].submit
+    print(cmd)
+    print(JOBS[-1].)
