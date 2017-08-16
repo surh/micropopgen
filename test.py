91a8dc07c282689c4a57d480968b2b138333a2aa
surh <sur00mx@gmail.com>
2017-08-16 14:27:35 -0700

    error

--- test.py
+++ test.py
@@ -21,7 +21,7 @@ for cmd in commands:
     out.append(fyrd.submit(cmd, runpath = "run", outpath = "out",
                            clean_files = False, clean_outputs = False,
                            mem = '10MB', nodes = 1, time ='00:30:00',
-                           threads = 1, partition = hbfraser))
+                           threads = 1, partition = 'hbfraser'))
     print(cmd) 
 
 # for i in out:
