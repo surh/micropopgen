d5aa831de4f4f50850ca0b3be15b340bf2d8033b
surh <sur00mx@gmail.com>
2017-08-16 14:14:45 -0700

    testing fyrd options

--- test.py
+++ test.py
@@ -3,12 +3,28 @@ import fyrd
 command1 = "ls -la"
 command2 = "sleep 10"
  
-job1  = fyrd.submit(command1, runpath = "run", outpath = "out",
-                    clean_files = False, clean_outputs = False)
-#print(job1)
-print(job1.get())
-#job2 = fyrd.submit(command2, depends=job1)
-#out  = job2.get()  # Will block until job completes
+# job1  = fyrd.submit(command1, runpath = "run", outpath = "out",
+#                     clean_files = False, clean_outputs = False)
+# print(job1)
+# print(job1.get())
+# job2 = fyrd.submit(command2, depends=job1)
+# out  = job2.get()  # Will block until job completes
+
+
+
+commands = ['ls -la /', 'ls -la /home/', 'pwd']
+out = []
+for cmd in commands:
+    out.append(fyrd.submit(cmd, runpath = "run", outpath = "out",
+                           clean_files = False, clean_outputs = False,
+                           mem = '10MB', nodes = 1, time ='00:30:00',
+                           threads = 1, partition = None))
+    print(cmd) 
+
+for i in out:
+    print(i.get())
+
+    
 
 
 
