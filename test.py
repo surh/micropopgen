2f7ac84f58916dc0432496c64cf305cd9b3415e9
surh <sur00mx@gmail.com>
2017-08-17 14:22:00 -0700

    testing having all jobs in one variable

--- test.py
+++ test.py
@@ -1,21 +1,37 @@
 #!/usr/bin/env python
 import fyrd
+import time
 
-commands = ['sleep 180','sleep 180','sleep 180',
-            'sleep 180', 'sleep 180', 'sleep 180']
+commands = ['sleep 60','sleep 60','sleep 60',
+            'sleep 60', 'sleep 60', 'sleep 60']
+
+def print_number(i):
+    print("== Start {}".format(str(i)))
+    time.sleep(30)
+    print("== End {}".format(str(i)))
+    return(i)
 
 JOBS = []
+iter = 0
 for cmd in commands:
-    JOBS.append(fyrd.Job(cmd, runpath = "run", outpath = "out",
-                         clean_files = False, clean_outputs = False,
-                         mem = '10MB', nodes = 1, time ='00:30:00',
-                         cores = 1, partition = 'hbfraser',
-                         scriptpath = 'sub', outfile = 'test.log',
-                         errfile = 'test.err'))
-     
-    (JOBS[-1]).submit(max_jobs = 3)
-    print(cmd)
-    print((JOBS[-1]).command)
+#     JOBS.append(fyrd.Job(cmd, runpath = "run", outpath = "out",
+#                          clean_files = False, clean_outputs = False,
+#                          mem = '10MB', nodes = 1, time ='00:30:00',
+#                          cores = 1, partition = 'hbfraser',
+#                          scriptpath = 'sub', outfile = 'test.log',
+#                          errfile = 'test.err'))
+    job = fyrd.Job(print_number(iter), runpath = "run", outpath = "out",
+                     clean_files = False, clean_outputs = False,
+                     mem = '10MB', nodes = 1, time ='00:30:00',
+                     cores = 1, partition = 'hbfraser',
+                     scriptpath = 'sub', outfile = 'test.log',
+                     errfile = 'test.err')
+    job.submit(max_jobs = 3)
+    iter = iter + 1
+         
+#     (JOBS[-1]).submit(max_jobs = 3)
+#     print(cmd)
+    #print((JOBS[-1]).command)
 
 
 # cmd = 'sleep 180'
