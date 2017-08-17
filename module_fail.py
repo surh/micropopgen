b83a8065e4a10a6884afa46045c50060f1d85974
surh <sur00mx@gmail.com>
2017-08-17 15:45:30 -0700

    testing script for module error

---
+++ module_fail.py
@@ -0,0 +1,8 @@
+import fyrd
+
+command = 'blastp -h'
+
+job = fyrd.Job(command, modules = "ncbi-blast+",
+               partition = owners, time = "00:30:00",
+               mem = "100MB")
+job.submit()
