bd9de92da28ea044200b25df938a1c6c482ed21d
surh <sur00mx@gmail.com>
2017-08-17 15:51:05 -0700

    typos

--- module_fail.py
+++ module_fail.py
@@ -4,6 +4,6 @@ import fyrd
 command = 'blastp -h'
 
 job = fyrd.Job(command, modules = "ncbi-blast+",
-               partition = owners, time = "00:30:00",
+               partition = 'owners', time = "00:30:00",
                mem = "100MB")
 job.submit()
