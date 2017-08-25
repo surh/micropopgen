bc5235cc332780f33c48877560283e1ed3a9e699
surh <sur00mx@gmail.com>
2017-08-25 16:22:28 -0700

    adding mktest

---
+++ mktest.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# Copyright (C) 2017 Sur Herrera Paredes
+
+import gzip
+#import sutilspy
+import os
+
+if __name__ == '__main__':
+    
+    indir = './'
+    
+    
+    sample_dirs = os.listdir(indir)
+    print(sample_dirs) 
+    
+    
+    
+
+
