189643bcb5144493510785b1af722003e9b18d1a
surh <sur00mx@gmail.com>
2017-09-25 16:01:50 -0700

    Converting to executable script. Added command line arguments via argparse

--- analyze_midas_snps/run_all_comparisons.py
+++ analyze_midas_snps/run_all_comparisons.py
@@ -1,8 +1,5 @@
-
-# coding: utf-8
-
-# In[31]:
-
+#!/usr/bin/env python
+# Copyright (C) 2017 Sur Herrera Paredes
 
 import pandas as pd
 import argparse
@@ -10,102 +7,72 @@ import os
 import sutilspy
 
 
-# In[18]:
-
-
-comparisons_file = '/home/sur/micropopgen/exp/2017/today4/comparisons.txt'
-indir = '/godot/hmp/midas/merged.snps/'
-map_file = '/home/sur/micropopgen/exp/2017/today4/full_map.txt'
-mk_bin = '/home/sur/micropopgen/src/micropopgen/analyze_midas_snps/MKtest.py'
-outdir = './out/'
-
-# parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-# required = parser.add_argument_group("Required arguments")
-# required.add_argument("--comparisons", help = "File with comparisons to make",
-#                       type = str, required = True)
-# #     required.add_argument("--metadata_file", help = "Mapping file for samples", type = str,
-# #                           required = True)
-# #     required.add_argument("--group1", help = "Group1 of comparison", type = str,
-# #                           required = True)
-# #     required.add_argument("--group2", help = "Group2 of comparison",
-# #                           required = True)
+if __name__ == '__main__':
+    # comparisons_file = '/home/sur/micropopgen/exp/2017/today4/comparisons.txt'
+    # indir = '/godot/hmp/midas/merged.snps/'
+    # map_file = '/home/sur/micropopgen/exp/2017/today4/full_map.txt'
+    # mk_bin = '/home/sur/micropopgen/src/micropopgen/analyze_midas_snps/MKtest.py'
+    # outdir = './out/'
+    
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    required = parser.add_argument_group("Required arguments")
+    required.add_argument("--comparisons_file", help = "File with comparisons to make",
+                          type = str, required = True)
+    required.add_argument("--indir", help = "Directory wher output from merge_midas is stored. One subdirectory per species",
+                   type = str, required = True)
+    required.add_argument('--map_file', help = "Mapping tab-delimited file, must contain and ID and Groups columns",
+                      type = str, required = True)
+    required.add_argument('--mk_bin', help = 'Executable for MKtest.py',
+                    type = str, required = True)
+    required.add_argument('--outdir', help = 'Output directory. Will create a subdirectory per species')
+    args = parser.parse_args()
         
-# #     parser.add_argument("--test", help = "Eventually specify test to perform",
-# #                         default = "G", type = str)
-# #     parser.add_argument("--outfile", help = "Output file with results",
-# #                    default = "mk_results.txt", type = str)
-# #     parser.add_argument("--min_count", help = "min depth at a position in a sample to consider that sample in that position",
-# #                         default = 1, type = int)
-# #     parser.add_argument("--nrows", help = "Number of gene positions to read",
-# #                         default = float('inf'), type = float)
-# #     parser.add_argument("--tables", help = "Output file for contingency tables",
-# #                         default = "mk_tables.txt", type = str)
-# #     parser.add_argument("--pseudocount", help = "Pseudocount value to use in contingency tables",
-# #                         default = 1, type = int)
-# args = parser.parse_args()
+    # Read list of comparisons
+    comparisons = pd.read_csv(comparisons_file, sep="\t")
+    comparisons.head()
     
-
-
-# In[19]:
-
-
-comparisons = pd.read_csv(comparisons_file, sep="\t")
-comparisons.head()
-
-
-# In[30]:
-
-
-for i, r in comparisons.iterrows():
-    #print(i)
-    #print(r)
-    
-    species_indir = ''.join([indir,'/',r['Species'], '/'])
-    #print(species_dir)
-    
-    # Create output directory
-    species_outdir = ''.join([outdir,'/',r['Species'],'/'])
-    try:
-        os.mkdir(species_outdir)
-        print("Creating output directory")
-    except FileExistsError:
-        print("Directory already exists")
-    
-    suffix = r['A'] + '_' + r['B']
-    suffix = suffix.replace(' ','.')
-    #print(suffix)
+    # For every comparison
+    for i, r in comparisons.iterrows():
+        #print(i)
+        #print(r)
         
-    species_outfile = ''.join([species_outdir,
-                               '/','mk_results.',
-                               suffix,'.txt'])
-    species_tables = ''.join([species_outdir,'/',
-                              'mk_tables.',suffix,
-                              '.txt'])
-    species_log = ''.join([species_outdir,'/',
-                           suffix,'.log'])
-    species_err = ''.join([species_outdir,'/',
-                           suffix,'.err'])
+        species_indir = ''.join([indir,'/',r['Species'], '/'])
+        #print(species_dir)
+        
+        # Create output directory
+        species_outdir = ''.join([outdir,'/',r['Species'],'/'])
+        try:
+            os.mkdir(species_outdir)
+            print("Creating output directory")
+        except FileExistsError:
+            print("Directory already exists")
+        
+        suffix = r['A'] + '_' + r['B']
+        suffix = suffix.replace(' ','.')
+        #print(suffix)
+            
+        species_outfile = ''.join([species_outdir,
+                                   '/','mk_results.',
+                                   suffix,'.txt'])
+        species_tables = ''.join([species_outdir,'/',
+                                  'mk_tables.',suffix,
+                                  '.txt'])
+        species_log = ''.join([species_outdir,'/',
+                               suffix,'.log'])
+        species_err = ''.join([species_outdir,'/',
+                               suffix,'.err'])
+        
+        cmd = [mk_bin, '--indir', species_dir,
+              '--metadata_file', map_file,
+               '--group1', r['A'],
+               '--group2', r['B'],
+               '--outfile', species_outfile,
+               '--tables', species_tables,
+               '1>', species_log,
+               '2>', species_err]
+        cmd = ' '.join(cmd)
+        #print(cmd)
+        sutilspy.io.run_command(cmd)
+        
+        #/micropopgen/src/micropopgen/analyze_midas_snps/MKtest.py --indir /godot/hmp/midas/merged.snps/Porphyromonas_sp_57899/ --metadata_file map.txt --group1 'Supragingival plaque' --group2 'Tongue dorsum' --outfile Porphyromonas_sp_57899_mk_results.txt --tables Porphyromonas_sp_57899_mk_tables.txt 1> log 2> err
     
-    cmd = [mk_bin, '--indir', species_dir,
-          '--metadata_file', map_file,
-           '--group1', r['A'],
-           '--group2', r['B'],
-           '--outfile', species_outfile,
-           '--tables', species_tables,
-           '1>', species_log,
-           '2>', species_err]
-    cmd = ' '.join(cmd)
-    #print(cmd)
-    sutilspy.io.run_command(cmd)
-    
-    #/micropopgen/src/micropopgen/analyze_midas_snps/MKtest.py --indir /godot/hmp/midas/merged.snps/Porphyromonas_sp_57899/ --metadata_file map.txt --group1 'Supragingival plaque' --group2 'Tongue dorsum' --outfile Porphyromonas_sp_57899_mk_results.txt --tables Porphyromonas_sp_57899_mk_tables.txt 1> log 2> err
-
-    
-
-
-# In[ ]:
-
-
-i
-
-
