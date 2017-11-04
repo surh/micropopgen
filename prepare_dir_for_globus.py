53eded2eab68fcf9b6f7916f5aebd8eb8504c423
surh <sur00mx@gmail.com>
2017-11-03 17:42:43 -0700

    Moving project to atom from eclipse. Checks existence of files and is consistent with python linter

--- prepare_dir_for_globus.py
+++ prepare_dir_for_globus.py
@@ -4,33 +4,53 @@ import argparse
 import os
 import sutilspy
 
-def check_sample(sample,path,extension = '.fastq.bz2'):
+
+def check_sample(sample, path, extension='.fastq.bz2'):
     """Check whether the files for a given sample are present"""
-    
-    r1_file = ''.join([path, sample, '_read1', extension])
-    r1_file = ''.join([path, sample, '_read1', extension])
+    # Create file names
+    r1_file = ''.join([path, '/', sample, '_read1', extension])
+    r2_file = ''.join([path, '/', sample, '_read2', extension])
+
+    # Check that files exist
+    try:
+        if not os.path.isfile(r1_file):
+            raise FileNotFoundError
+        if not os.path.isfile(r2_file):
+            raise FileNotFoundError
+    except:
+        print("""ERROR: At least one of the files for
+              sample {} is missing""".format(sample))
+        raise
 
 
 if __name__ == '__main__':
     # Read arguments
-    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser_format = argparse.ArgumentDefaultsHelpFormatter
+    parser = argparse.ArgumentParser(formatter_class=parser_format)
     required = parser.add_argument_group("Required arguments")
-    required.add_argument("--indir", help = "Path to directory where fastq files are located",
-                          required = True)
-    required.add_argument("--outdir", help = "Path to directory where to create links",
-                          required = True)
-    required.add_argument("--samples", help = 'File containing sample IDs to be linked. Must be one entry per line',
-                          required = True)
-    parser.add_argument("--overwrite", help = "Flag indicating that any existing file must be overwritten",
-                        action = "store_true")
-    
+    required.add_argument("--indir", help="""Path to directory where fastq
+                          files are located""",
+                          required=True)
+    required.add_argument("--outdir", help="""Path to directory where to
+                          create links""",
+                          required=True)
+    required.add_argument("--samples", help="""File containing sample IDs
+                          to be linked. Must be one entry per line""",
+                          required=True)
+    parser.add_argument("--overwrite", help="""Flag indicating that any
+                        existing file must be overwritten""",
+                        action="store_true")
+
     args = parser.parse_args()
-    
+
     # Read list of samples
     try:
-        samples = sutilspy.io.return_column(args.samples,1,header = False)
-        #print(samples)
+        samples = sutilspy.io.return_column(args.samples, 1, header=False)
+        # print(samples)
     except:
         print("ERROR: Failed reading sample list")
         raise
-    
+
+    # Check every sample
+    for s in samples:
+        check_sample(s, args.indir)
