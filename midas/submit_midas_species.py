99f14b72f1c24703f26114ca26336a5d9b3c1cba
surh <sur00mx@gmail.com>
2017-12-01 12:10:40 -0800

    Changed from MIDAS/1.2.1 to MIDAS/1.3.1

--- midas/submit_midas_species.py
+++ midas/submit_midas_species.py
@@ -5,10 +5,10 @@ import os
 
 if __name__ == "__main__":
     import argparse
-    
+
     # Parse arguments
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    
+
     # Required arguments
     required = parser.add_argument_group("Required arguments")
     required.add_argument("--samples", help = "File with samples (one per line) to be processed",
@@ -17,7 +17,7 @@ if __name__ == "__main__":
                           type = str, required = True)
     required.add_argument("--outdir", help = "Directory where to save output",
                           type = str, required = True)
-    
+
     # Optional arguments
     parser.add_argument("--sample_col",help = "Column where sample id is located in --samples",
                         default = 1, type = int)
@@ -32,63 +32,63 @@ if __name__ == "__main__":
     parser.add_argument("--memory", help = "Amount of memory to request", default = "10G", type = str)
     parser.add_argument("--time", help = "If method is slurm, amount of time to reserve",
                         type = str, default = "4:00:00")
-    
+
     args = parser.parse_args()
     #args.sample_col -= 1
-    
-    
+
     # Read samples
-    samples = sutilspy.io.return_column(infile = args.samples,
-                                        col = args.sample_col,
-                                        separator = '\t',
-                                        header = False)
+    samples = sutilspy.io.return_column(infile=args.samples,
+                                        col=args.sample_col,
+                                        separator='\t',
+                                        header=False)
+
     # Prepare directories
-    if args.method in ['qsub','slurm']:
+    if args.method in ['qsub', 'slurm']:
         if not os.path.isdir(args.logdir):
             print("Creating directory {}".format(args.logdir))
             os.mkdir(args.logdir)
         if not os.path.isdir(args.submissions_dir):
             print("Creating directory {}".format(args.submissions_dir))
             os.mkdir(args.submissions_dir)
-    
-    
-    #print(samples)
-    
+
+    # print(samples)
+
     pre_commands = []
-    
+
     # Add module dependencies
-    pre_commands.append("module load MIDAS/1.2.1")
+    # pre_commands.append("module load MIDAS/1.2.1")
+    pre_commands.append("module load MIDAS/1.3.1")
     pre_commands.append("echo MIDAS database is $MIDAS_DB")
     bin = "run_midas.py"
-    
+
     for sample in samples:
         print("== Processing sample {}".format(sample))
         sample_file_base = args.indir + "/" + sample
         read1 = sample_file_base + "_read1.fastq.bz2"
         read2 = sample_file_base + "_read2.fastq.bz2"
-        
+
         if not os.path.isfile(read1):
             raise FileNotFoundError("File {} not found".format(read1))
         if not os.path.isfile(read2):
             raise FileNotFoundError("File {} not found".format(read2))
-        
-        midas_command = [bin,"species",args.outdir + "/" + sample,
-                         "-1", read1, "-2", read2,"-t","8",
+
+        midas_command = [bin, "species", args.outdir + "/" + sample,
+                         "-1", read1, "-2", read2, "-t", "8",
                          "--remove_temp"]
         midas_command = " ".join(midas_command)
-        #print("#######")
-        #print(midas_command)
-        #print("#######")
-        
+        # print("#######")
+        # print(midas_command)
+        # print("#######")
+
         commands = pre_commands[:]
         commands.append(midas_command)
-        
-        job_name = sample + ".midas"     
+
+        job_name = sample + ".midas"
         logfile = args.logdir + "/midas.species." + sample + ".log"
         errorfile = args.logdir + "/midas.species." + sample + ".err"
-   
+
         submission_file = args.submissions_dir + "/midas.species." + sample + ".bash"
-        
+
         with open(submission_file,'w') as fh:
             if args.method == 'qsub':
                 #memory = "16000mb"
@@ -117,7 +117,7 @@ if __name__ == "__main__":
                 raise ValueError("Invalid method {}".format(args.method))
         fh.close()
         os.chmod(submission_file, 0o744)
-        
+
         # Submit submission file
         if args.method == 'qsub':
             #print(submission_file)
@@ -129,4 +129,3 @@ if __name__ == "__main__":
             sutilspy.io.run_command(submission_file)
         else:
             raise ValueError("Incorrect method supplie ({})".format(args.method))
-    
