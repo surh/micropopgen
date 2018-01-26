1c04c61245f4c7c01f512567a34dc1484132ef95
surh <sur00mx@gmail.com>
2018-01-26 11:46:37 -0800

    preparing to take command line args

--- midas/analyze_midas_snps/find_comparisons.r
+++ midas/analyze_midas_snps/find_comparisons.r
@@ -1,8 +1,16 @@
 library(AMOR)
-tab <- read.am("~/micropopgen/exp/2017/2017-08-07.abundances/merge.species/coverage.txt")
+
+# covfile <- commandArgs(trailingOnly = TRUE)[1]
+# mapfile <- commandArgs(trailingOnly = TRUE)[2]
+covfile <- opts[1]
+mapfile <- opts[2]
+
+# tab <- read.am("~/micropopgen/exp/2017/2017-08-07.abundances/merge.species/coverage.txt")
+tab <- read.am(covfile)
 tab
 
-map <- read.table("full_map.txt", header = TRUE, sep ="\t")
+# map <- read.table("full_map.txt", header = TRUE, sep ="\t")
+map <- read.table(mapfile, header = TRUE, sep ="\t")
 head(map)
 row.names(map) <- as.character(map$ID)
 
