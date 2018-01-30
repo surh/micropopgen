c69c272b2e4ed4e6ef6e175dad3ac1446ba24c65
surh <sur00mx@gmail.com>
2018-01-29 16:46:52 -0800

    adding number of plymorphyc

--- midas/analyze_midas_snps/summarize_mk_results.py
+++ midas/analyze_midas_snps/summarize_mk_results.py
@@ -25,11 +25,12 @@ if __name__ == "__main__":
                 n_Dn = sum(dat.Dn > 0)
                 n_Ds = sum(dat.Ds > 0)
                 n_D = sum((dat.Dn + dat.Ds) > 0)
+                n_P = sum((dat.Pn + dat.Ps) > 0)
                 n_p = sum(dat.hg_p < 0.1)
                 n_p2 = sum(dat.hg_p < 0.05)
 
                 res = [d, f, str(ngenes), str(n_Dn),
-                       str(n_Ds), str(n_D),
+                       str(n_Ds), str(n_D), str(n_P)
                        str(n_p), str(n_p2)]
                 # print(d)
                 # print(f)
@@ -42,7 +43,7 @@ if __name__ == "__main__":
     print(Res)
     with open('summary.txt', 'w') as o:
         o.write("\t".join(['Strain', 'Comparison', 'ngenes', 'n_Dn',
-                           'n_Ds',
-                           'n_D', 'p0.1', 'p0.05']) + "\n")
+                           'n_Ds', 'n_D', 'n_P',
+                           'p0.1', 'p0.05']) + "\n")
         for r in Res:
             o.write("\t".join(r) + "\n")
