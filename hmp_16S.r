51f33bf8aedbbc6e6a9781b1cdf3c77819f41716
surh <sur00mx@gmail.com>
2018-03-05 18:11:03 -0800

    Calculate prevalence

--- hmp_16S.r
+++ hmp_16S.r
@@ -115,10 +115,36 @@ Dat <- subset(Dat, body_site %in% c("Buccal mucosa", "Supragingival plaque", "To
 #' 
 #' @author Sur Herrera Paredes
 #' 
+#' @importFrom reshape2 melt
 calculate_prevalence <- function(Dat, thres = 1, group = NULL){
   if(class(Dat) != "Dataset")
     stop("ERROR: A Dataset object must be passed", call. = TRUE )
   
+  if(is.null(group)){
+    group <- rep("All", length.out = ncol(Dat.bin$Tab))
+    group_n <- table(group)
+    varnames <- c("Group", "Taxon")
+  }else{
+    group <- Dat$Map[ , group ]
+    group_n <- table(group)
+    varnames <- c("Taxon", "Group")
+  }
+  
+  Dat.bin <- create_dataset(Tab = 1*(Dat$Tab >= thres), Map = Dat$Map, Tax = Dat$Tax)
+  Dat.bin <- pool_samples.default(Tab = Dat.bin$Tab, groups = group, FUN = sum)
+  
+  # res <- data.frame(Taxon = row.names(Dat$Tab), Count = rowSums(Dat$Tab >= thres))
+  # res$Proportion <- res$Count / ncol(Dat$Tab)
+  
+  Res <- reshape2::melt(Dat.bin$Tab, varnames = varnames, value.name = "Count")
+  Res <- Res[ , c("Taxon", "Group", "Count") ]
+  
+  if(length(group_n) == 1)
+    Res$Group <- "All"
+  
+  Res$Proportion <- as.vector(Res$Count / group_n[ as.character(Res$Group) ])
+  
+  return(Res)
 }
 
 
