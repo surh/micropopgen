fb5248a8fa5b7e6693e82063c4cf83f7191144a3
surh <sur00mx@gmail.com>
2018-04-02 16:20:21 -0700

    waiting

--- bacteria/marker_finder.py
+++ bacteria/marker_finder.py
@@ -20,7 +20,7 @@ import fyrd
 import argparse
 import os
 from Bio import SearchIO
-# import time
+import time
 
 def process_arguments():
     # Read arguments
@@ -207,5 +207,5 @@ if __name__ == "__main__":
     for f, j in hmm_files.items():
         print(f)
         job = fyrd.Job(get_hmm_hits(f), depends=j, runpath=os.getcwd())
-        # time.sleep(5)
+        time.sleep(15)
         job.submit(max_jobs=args.maxjobs)
