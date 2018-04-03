07df9fa81ecd045b5118e9b82fd7d46233631b23
surh <sur00mx@gmail.com>
2018-04-02 19:41:23 -0700

    testing depends

--- test_fyrd.py
+++ test_fyrd.py
@@ -3,6 +3,6 @@ import os
 import time
 
 
-job1 = fyrd.Job('sleep 10; echo "Hello" > myfile.test', runpath=os.getcwd())
+job1 = fyrd.Job('sleep 5; echo "Hello" > myfile.test', runpath=os.getcwd())
 job1.submit()
-job2 = fyrd.Job('ls -l myfile.test', runpath=os.getcwd())
+job2 = fyrd.Job('ls -l myfile.test', runpath=os.getcwd(),depends=job1)
