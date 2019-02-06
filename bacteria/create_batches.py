ac8cababe7b8b69fc5a72813ee7d9ce018f0bd7c
surh <sur00mx@gmail.com>
2019-02-06 12:24:55 -0800

    copyright

--- bacteria/create_batches.py
+++ bacteria/create_batches.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Copyright (C) 2018 Sur Herrera Paredes
+# Copyright (C) 2018-2019 Sur Herrera Paredes
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
