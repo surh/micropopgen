2556f293bed7bc397478e51e016446db29f10ce5
surh <sur00mx@gmail.com>
2020-08-28 16:23:29 -0700

    setting diploid to unphased

--- bacteria/UHGG/uhgg_snv2vcf.py
+++ bacteria/UHGG/uhgg_snv2vcf.py
@@ -203,7 +203,7 @@ def tsv2vcf(snv_file, genome_fasta, outfile='snvs.vcf', include_genomes=False,
             if include_genomes:
 
                 if write_diploid:
-                    vcf_genotypes = ['.' if g == '255' else g + '|' + g for g in genotypes]
+                    vcf_genotypes = ['.' if g == '255' else g + '/' + g for g in genotypes]
                 else:
                     vcf_genotypes = ['.' if g == '255' else g for g in genotypes]
 
